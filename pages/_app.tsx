ecc15b223869b8ad1c6916462631eca050b1ae8d
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-02 15:39:48 +0900

    import and rap MantineProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { MantineProvider } from '@mantine/core'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colorScheme: 'dark',
+        fontFamily: 'Verdana, sans-serif',
+      }}
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  )
 }
 
 export default MyApp
