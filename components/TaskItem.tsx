26a9a27dcc181231eab62e5fc210d74e4db956f4
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-04 00:34:41 +0900

    set arg props

--- components/TaskItem.tsx
+++ components/TaskItem.tsx
@@ -5,6 +5,11 @@ import { PencilAltIcon, TrashIcon } from '@heroicons/react/solid'
 import useStore from '../store'
 import { useMutateTask } from '../hooks/useMutateTask'
 
-export const TaskItem: React.FC = () => {
+export const TaskItem: React.FC<
+  Omit<Task, 'createdAt' | 'updatedAt' | 'userId'>
+> = ({ id, title, description }) => {
+  const update = useStore((state) => state.updateEditedTask)
+  const { deleteTaskMutation } = useMutateTask()
+
   return <div>TaskItem</div>
 }
