85dbc6e897685204e5ce2cbd65fa3f4764a430d6
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-03 16:29:14 +0900

    add useQueryClient

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
+import { useQueryClient } from '@tanstack/react-query'
 import axios from 'axios'
 import { LogoutIcon } from '@heroicons/react/outline'
 import { Layout } from '../components/Layout'
@@ -7,8 +8,10 @@ import { UserInfo } from '../components/UserInfo'
 
 const Dashboard: NextPage = () => {
   const router = useRouter()
+  const queryClient = useQueryClient()
 
   const logout = async (): Promise<void> => {
+    queryClient.removeQueries(['user'])
     await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`)
     router.push('/')
   }
