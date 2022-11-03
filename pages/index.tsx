1221ab8045772ee4a96a8e0a46c4c45a4c5ef564
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-03 15:05:17 +0900

    import modules

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,19 @@ import { useRouter } from 'next/router'
 import axios from 'axios'
 import * as Yup from 'yup'
 import { IconDatabase } from '@tabler/icons'
-import {ShieldCheckIcon} from '@heroicons/react/outline'
-import {ExclamationCircleIcon} from '@heroicons/react/outline'
-import { Anchor, TextInput, Button, Group, PasswordInput, Alert } from '@mantine/core'
+import { ShieldCheckIcon } from '@heroicons/react/outline'
+import { ExclamationCircleIcon } from '@heroicons/react/outline'
+import {
+  Anchor,
+  TextInput,
+  Button,
+  Group,
+  PasswordInput,
+  Alert,
+} from '@mantine/core'
+import { useForm, yupResolver } from '@mantine/form'
+import { Layout } from '../components/Layout'
+import { AuthForm } from '../types'
 
 const Home: NextPage = () => {
   return <div></div>
