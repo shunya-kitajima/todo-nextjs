5fe891b263fff17d9c3e4d085c1be1acbd828bcc
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-04 00:55:15 +0900

    display tasks

--- components/TaskList.tsx
+++ components/TaskList.tsx
@@ -5,7 +5,29 @@ import { useQueryTasks } from '../hooks/useQueryTasks'
 import { TaskItem } from './TaskItem'
 
 export const TaskList: React.FC = () => {
-  const { data, status } = useQueryTasks()
+  const { data: tasks, status } = useQueryTasks()
 
-  return <div>TaskList</div>
+  if (status === 'loading') return <Loader my="lg" color="cyan" />
+
+  return (
+    <List
+      my="lg"
+      spacing="sm"
+      size="sm"
+      icon={
+        <ThemeIcon color="cyan" size={24} radius="xl">
+          <IconCircleDashed size={16} />
+        </ThemeIcon>
+      }
+    >
+      {tasks?.map((task) => (
+        <TaskItem
+          key={task.id}
+          id={task.id}
+          title={task.title}
+          description={task.description}
+        />
+      ))}
+    </List>
+  )
 }
