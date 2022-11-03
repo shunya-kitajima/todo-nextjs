5d18ff6db367fae0e6e40faf597b12aa4e31add4
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-04 00:29:49 +0900

    import modules

---
+++ components/TaskItem.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Task } from '@prisma/client'
+import { List } from '@mantine/core'
+import { PencilAltIcon, TrashIcon } from '@heroicons/react/solid'
+import useStore from '../store'
+import { useMutateTask } from '../hooks/useMutateTask'
+
+export const TaskItem: React.FC = () => {
+  return <div>TaskItem</div>
+}
