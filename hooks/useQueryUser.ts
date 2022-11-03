b297ee49cbb079af31300a23ffc80e46d824c611
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-03 16:08:41 +0900

    import modules

---
+++ hooks/useQueryUser.ts
@@ -0,0 +1,8 @@
+import { useRouter } from 'next/router'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import { User } from '@prisma/client'
+
+export const useQueryUser = () => {
+  const getUser = async () => {}
+}
