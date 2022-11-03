fe3c47437cee63a59a3ab46d0c7d166acbdbde1a
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-04 00:16:11 +0900

    create state and handlesubmit

--- components/TaskForm.tsx
+++ components/TaskForm.tsx
@@ -5,5 +5,21 @@ import useStore from '../store'
 import { useMutateTask } from '../hooks/useMutateTask'
 
 export const TaskForm: React.FC = () => {
+  const { editedTask } = useStore()
+  const update = useStore((state) => state.updateEditedTask)
+  const { createTaskMutation, updateTaskMutation } = useMutateTask()
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (editedTask.id === 0) {
+      createTaskMutation.mutate({
+        title: editedTask.title,
+        description: editedTask.description,
+      })
+    } else {
+      updateTaskMutation.mutate(editedTask)
+    }
+  }
+
   return <div>TaskForm</div>
 }
