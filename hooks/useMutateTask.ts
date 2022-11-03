335db40bdc2d9f15db3cfaf5f048c97bb0129cce
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-03 21:19:36 +0900

    create createTaskMutation

--- hooks/useMutateTask.ts
+++ hooks/useMutateTask.ts
@@ -8,8 +8,33 @@ import { EditedTask } from '../types'
 export const useMutateTask = () => {
   const router = useRouter()
   const queryClient = useQueryClient()
-  const update = useStore((state) => state.updateEditedTask)
   const reset = useStore((state) => state.resetEditedTask)
+
+  const createTaskMutation = useMutation(
+    async (task: Omit<EditedTask, 'id'>) => {
+      const res = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/todo`,
+        task
+      )
+      return res.data
+    },
+    {
+      onSuccess: (res: any) => {
+        const previousTasks = queryClient.getQueryData<Task[]>(['tasks'])
+        if (previousTasks) {
+          queryClient.setQueryData<Task[]>(
+            ['tasks'],
+            [res.data, ...previousTasks]
+          )
+        }
+        reset()
+      },
+      onError: (err: any) => {
+        if (err.response.status === 401 || err.response.status === 403)
+          router.push('/')
+      },
+    }
+  )
 
   return {}
 }
