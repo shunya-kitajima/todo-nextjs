616601c8e12a2c5f1fa8e8427fe6e0ac5df15eb4
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-03 16:16:27 +0900

    create useQueryUser

--- hooks/useQueryUser.ts
+++ hooks/useQueryUser.ts
@@ -4,5 +4,21 @@ import axios from 'axios'
 import { User } from '@prisma/client'
 
 export const useQueryUser = () => {
-  const getUser = async () => {}
+  const router = useRouter()
+
+  const getUser = async (): Promise<Omit<User, 'hashedPassword'>> => {
+    const { data } = await axios.get<Omit<User, 'hashedPassword'>>(
+      `${process.env.NEXT_PUBLIC_API_URL}/user`
+    )
+    return data
+  }
+
+  return useQuery<Omit<User, 'hashedPassword'>, Error>({
+    queryKey: ['user'],
+    queryFn: getUser,
+    onError: (err: any) => {
+      if (err.response.status === 401 || err.response.status === 403)
+        router.push('/')
+    },
+  })
 }
